7798eda0ba41a515defcee42172e5871c5c4e5a2
Robot-Inventor <30.keita@gmail.com>
2024-11-07 22:30:56 +0900

    URLの末尾のスラッシュの有無のどちらでもアクセスできるように変更

--- src/ts/build.ts
+++ src/ts/build.ts
@@ -6,6 +6,28 @@ import path from "node:path";
 const OUTPUT_DIRECTORY = "./dist";
 const OUTPUT_PATH = path.join(OUTPUT_DIRECTORY, "_redirects");
 
+const modifiedLinks: Array<(typeof links)[number]> = [];
+
+for (const link of links) {
+    if (link.source === "/") continue;
+
+    if (link.source.endsWith("/")) {
+        modifiedLinks.push({
+            source: link.source.slice(0, -1),
+            destination: link.destination,
+            code: link.code
+        });
+    } else {
+        modifiedLinks.push({
+            source: `${link.source}/`,
+            destination: link.destination,
+            code: link.code
+        });
+    }
+}
+
+links.push(...modifiedLinks);
+
 const main = async () => {
     const redirects = links.map((link) => `${link.source} ${link.destination} ${link.code ?? 302}`);
     const redirectsString = `${redirects.join("\n")}\n`;
